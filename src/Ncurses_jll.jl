2009c5f17af370cdd22dff58dca6006109c50c19
Elliot Saba <staticfloat@gmail.com>
2019-10-02 17:59:33 -0400

    Ncurses_jll build 6.1.0+0

---
+++ src/Ncurses_jll.jl
@@ -0,0 +1,31 @@
+module Ncurses_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("68e3532b-a499-55ff-9963-d1c0c0748b3a"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Ncurses", artifacts_toml) for e in artifacts["Ncurses"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Ncurses; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Ncurses_jll
