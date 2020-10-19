9fc336f5ce216630cc252fd2e7fdefebf9fa231f
jlbuild <juliabuildbot@gmail.com>
2020-10-19 12:28:09 +0000

    Ncurses_jll build 6.1.0+5

--- src/wrappers/armv7l-linux-musleabihf.jl
+++ src/wrappers/armv7l-linux-musleabihf.jl
@@ -2,121 +2,36 @@
 export libform, libmenu, libncurses, libpanel
 
 using Gettext_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libform`
-const libform_splitpath = ["lib", "libform.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libform_path = ""
-
-# libform-specific global declaration
-# This will be filled out by __init__()
-libform_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libform = "libformw.so.6"
-
-
-# Relative path to `libmenu`
-const libmenu_splitpath = ["lib", "libmenu.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libmenu_path = ""
-
-# libmenu-specific global declaration
-# This will be filled out by __init__()
-libmenu_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libmenu = "libmenuw.so.6"
-
-
-# Relative path to `libncurses`
-const libncurses_splitpath = ["lib", "libncurses.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libncurses_path = ""
-
-# libncurses-specific global declaration
-# This will be filled out by __init__()
-libncurses_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libncurses = "libncursesw.so.6"
-
-
-# Relative path to `libpanel`
-const libpanel_splitpath = ["lib", "libpanel.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libpanel_path = ""
-
-# libpanel-specific global declaration
-# This will be filled out by __init__()
-libpanel_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libpanel = "libpanelw.so.6"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Ncurses")
+JLLWrappers.@declare_library_product(libform, "libformw.so.6")
+JLLWrappers.@declare_library_product(libmenu, "libmenuw.so.6")
+JLLWrappers.@declare_library_product(libncurses, "libncursesw.so.6")
+JLLWrappers.@declare_library_product(libpanel, "libpanelw.so.6")
 function __init__()
-    global artifact_dir = abspath(artifact"Ncurses")
+    JLLWrappers.@generate_init_header(Gettext_jll)
+    JLLWrappers.@init_library_product(
+        libform,
+        "lib/libform.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Gettext_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Gettext_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libmenu,
+        "lib/libmenu.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libform_path = normpath(joinpath(artifact_dir, libform_splitpath...))
+    JLLWrappers.@init_library_product(
+        libncurses,
+        "lib/libncurses.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libform_handle = dlopen(libform_path)
-    push!(LIBPATH_list, dirname(libform_path))
+    JLLWrappers.@init_library_product(
+        libpanel,
+        "lib/libpanel.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libmenu_path = normpath(joinpath(artifact_dir, libmenu_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libmenu_handle = dlopen(libmenu_path)
-    push!(LIBPATH_list, dirname(libmenu_path))
-
-    global libncurses_path = normpath(joinpath(artifact_dir, libncurses_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libncurses_handle = dlopen(libncurses_path)
-    push!(LIBPATH_list, dirname(libncurses_path))
-
-    global libpanel_path = normpath(joinpath(artifact_dir, libpanel_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libpanel_handle = dlopen(libpanel_path)
-    push!(LIBPATH_list, dirname(libpanel_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
